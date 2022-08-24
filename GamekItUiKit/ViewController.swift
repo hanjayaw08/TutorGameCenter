8ae5b36d682b5281f20eabc35beb38c0e9505384
hanjaya888 <57930023+hanjaya888@users.noreply.github.com>
2022-08-24 20:46:34 +0700

    Initial Commit

---
+++ GamekItUiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamekItUiKit
+//
+//  Created by Hanjaya Putra Wijangga on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
